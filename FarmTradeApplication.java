00435b8db435b4a254e81a6887fb21ae7bebd595
tlpavan <lakshmanapavankalyant@gmail.com>
2026-09-05 00:09:01 +0530

    Add files via upload

---
+++ FarmTradeApplication.java
@@ -0,0 +1,11 @@
+package com.farmtrade.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmTradeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FarmTradeApplication.class, args);
+    }
+}
